bec1b7f9af529293a5f35f1c8f136a7706d89514
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-09-20 12:06:23 +0200

    restructure tests

--- tests/medium.rs
+++ tests/medium.rs
@@ -14,3 +14,17 @@ fn test_just_one_string_trailing_comma() {
 
   assert_eq!(parse_json(input), expected);
 }
+
+#[test]
+fn test_bigger_object() {
+  let input = r##"{"a_string":"Hello world!","an_integer":17,"a_float":3.14,"a_true_bool":true,"a_false_bool":false}"##;
+
+  let mut expected = HashMap::new();
+  expected.insert("a_string", JsonValue::String("Hello world!"));
+  expected.insert("an_integer", JsonValue::Number(17.0));
+  expected.insert("a_float", JsonValue::Number(3.14));
+  expected.insert("a_true_bool", JsonValue::Boolean(true));
+  expected.insert("a_false_bool", JsonValue::Boolean(false));
+
+  assert_eq!(parse_json(input), expected);
+}
--- tests/nested.rs
+++ tests/nested.rs
@@ -3,28 +3,6 @@ use json_parser::*;
 use std::collections::HashMap;
 
 #[test]
-fn test_just_one_string() {
-  let input = r##"{"a_string":"Hello world!"}"##;
-
-  let mut expected = HashMap::new();
-  expected.insert("a_string", JsonValue::String("Hello world!"));
-
-  assert_eq!(parse_json(input), expected);
-}
-
-#[test]
-fn test_just_one_string_beautified() {
-  let input = r##"{
-    "a_string": "Hello world!"
-}}"##;
-
-  let mut expected = HashMap::new();
-  expected.insert("a_string", JsonValue::String("Hello world!"));
-
-  assert_eq!(parse_json(input), expected);
-}
-
-#[test]
 fn test_nested_object_simple() {
   let input = r##"{"object_name":{"prop_name": "value in nested object"}}"##;
 
@@ -37,33 +15,6 @@ fn test_nested_object_simple() {
 
     top_level_obj
   };
-
-  assert_eq!(parse_json(input), expected);
-}
-
-#[test]
-fn test_just_one_string_trailing_comma() {
-  let input = r##"{
-    "a_string": "Hello world!",
-}}"##;
-
-  let mut expected = HashMap::new();
-  expected.insert("a_string", JsonValue::String("Hello world!"));
-
-  assert_eq!(parse_json(input), expected);
-}
-
-
-#[test]
-fn test_bigger_object() {
-  let input = r##"{"a_string":"Hello world!","an_integer":17,"a_float":3.14,"a_true_bool":true,"a_false_bool":false}"##;
-
-  let mut expected = HashMap::new();
-  expected.insert("a_string", JsonValue::String("Hello world!"));
-  expected.insert("an_integer", JsonValue::Number(17.0));
-  expected.insert("a_float", JsonValue::Number(3.14));
-  expected.insert("a_true_bool", JsonValue::Boolean(true));
-  expected.insert("a_false_bool", JsonValue::Boolean(false));
 
   assert_eq!(parse_json(input), expected);
 }
