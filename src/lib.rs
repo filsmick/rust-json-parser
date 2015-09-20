53f4e7c2b5eb74866e3be7c662cfe86a8cfe3789
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-09-20 11:18:41 +0200

    add test_bigger_object_nested_beautified_trailing_comma()

--- src/lib.rs
+++ src/lib.rs
@@ -282,8 +282,6 @@ fn test_just_one_string_trailing_comma() {
 }
 
 
-
-
 #[test]
 fn test_bigger_object() {
   let input = r##"{"a_string":"Hello world!","an_integer":17,"a_float":3.14,"a_true_bool":true,"a_false_bool":false}"##;
@@ -294,6 +292,43 @@ fn test_bigger_object() {
   expected.insert("a_float", JsonValue::Number(3.14));
   expected.insert("a_true_bool", JsonValue::Boolean(true));
   expected.insert("a_false_bool", JsonValue::Boolean(false));
+
+  assert_eq!(parse_json(input), expected);
+}
+
+#[test]
+fn test_bigger_object_nested_beautified_trailing_comma() {
+  let input = r##"{
+    "a_string": "Hello world!",
+    "an_integer": 17,
+    "a_float": 3.14,
+    "a_true_bool": true,
+    "a_false_bool": false,
+    "a_nested_object": {
+        "another_nested_object": {
+            "a_deeply_nested_property": 45.89
+        }
+    }
+}"##;
+
+
+  let expected = {
+    let mut second_nested_obj = HashMap::new();
+    second_nested_obj.insert("a_deeply_nested_property", JsonValue::Number(45.89));
+
+    let mut nested_obj = HashMap::new();
+    nested_obj.insert("another_nested_object", JsonValue::Object(second_nested_obj));
+
+    let mut top_level_obj = HashMap::new();
+    top_level_obj.insert("a_string", JsonValue::String("Hello world!"));
+    top_level_obj.insert("an_integer", JsonValue::Number(17.0));
+    top_level_obj.insert("a_float", JsonValue::Number(3.14));
+    top_level_obj.insert("a_true_bool", JsonValue::Boolean(true));
+    top_level_obj.insert("a_false_bool", JsonValue::Boolean(false));
+    top_level_obj.insert("a_nested_object", JsonValue::Object(nested_obj));
+
+    top_level_obj
+  };
 
   assert_eq!(parse_json(input), expected);
 }
