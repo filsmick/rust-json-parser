02a5bfd822e5bf449ea2caa7ba85e0f2bdf56e80
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-09-22 20:03:04 +0200

    include non-ascii chars in property name of unicode.rs test

--- tests/unicode.rs
+++ tests/unicode.rs
@@ -5,10 +5,10 @@ use std::borrow::Cow;
 
 #[test]
 fn hebrew_russian_ascii() {
-  let input = r##"{"a_non_ascii_string":"שלום привет hello"}"##;
+  let input = r##"{"привет world":"שלום привет hello"}"##;
 
   let mut expected = HashMap::new();
-  expected.insert(Cow::Borrowed("a_non_ascii_string"), JsonValue::String(Cow::Borrowed("שלום привет hello")));
+  expected.insert(Cow::Borrowed("привет world"), JsonValue::String(Cow::Borrowed("שלום привет hello")));
   let expected = JsonValue::Object(expected);
 
   assert_eq!(parse_json(input).unwrap(), expected);
